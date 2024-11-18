a9223e4bef37b017b1c1af434cb5c44d05359727
sam-abreu <168447703+sam-abreu@users.noreply.github.com>
2024-11-18 20:10:32 -0300

    Adicionando definições básicas em screen.h

--- screen.h
+++ screen.h
@@ -1,4 +1,15 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#endif /* SCREEN_H */
+#include <stdio.h>
+
+/* ANSI Terminal Control Sequences */
+#define ESC            "\033"
+#define NORMALTEXT     "[0m"
+#define BOLDTEXT       "[1m"
+#define HOMECURSOR     "[H"
+#define SHOWCURSOR     "[?25h"
+#define HIDECURSOR     "[?25l"
+#define CLEARSCREEN    "[2J"
+
+#endif / SCREEN_H */
