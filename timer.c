da42bc054a0bd86f036168d4536927674a00a97b
luisglauria <luisguilherme.lauria@gmail.com>
2024-11-18 18:46:17 -0300

    Adicionando a lógica para verificar se o tempo acabou (timerTimeOver) e implementando timerPrint para exibir informações do temporizador.

--- timer.c
+++ timer.c
@@ -10,3 +10,32 @@ void timerInit(int valueMilliSec)
     delay = valueMilliSec;
     gettimeofday(&timer, NULL);
 }
+
+void timerDestroy()
+{
+    delay = -1;
+}
+
+    void timerUpdateTimer(int valueMilliSec)
+    {
+        delay = valueMilliSec;
+        gettimeofday(&timer, NULL);
+    }
+
+int timerTimeOver()
+{
+    int ret = 0;
+
+    if (getTimeDiff() > delay)
+    {
+        ret = 1;
+        gettimeofday(&timer, NULL);
+    }
+
+    return ret;
+}
+
+void timerPrint()
+{
+    printf("Timer:  %d", getTimeDiff());
+}
