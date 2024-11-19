ec8d2e6769890290d0db41644dc8ea8b0c79c0fa
Guilherme Mourão <160972988+guilhermemouraovc@users.noreply.github.com>
2024-11-18 21:40:24 -0300

    Adição do arquivo principal com headers e definições básicas

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "keyboard.h"
+#include "screen.h"
+#include "timer.h"
+#include "score.h"
+
+#define COLUNAS 40
+#define LINHAS 19
+#define MAX_SCORES 5
+#define INCREMENTO_VELOCIDADE 5
+#define NUM_OBSTACULOS 5
+#define VELOCIDADE_MINIMA 20
